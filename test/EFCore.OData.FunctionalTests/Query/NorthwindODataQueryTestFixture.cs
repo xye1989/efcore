9d1411fde2e1945772c4b5b40e081b596c9e79e4
maumar <maumar@microsoft.com>
2020-08-26 17:24:13 -0700

    Fix to #21402 - Test using OData
    Adding infrastructure and rudimentary tests using OData. Ported Northwind, ComplexNavigations and GearsOfWar models. Only setup for sqlserver currently.
    
    Resolves #21402

---
+++ test/EFCore.OData.FunctionalTests/Query/NorthwindODataQueryTestFixture.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.EntityFrameworkCore.TestModels.Northwind;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OData.Edm;
+
+namespace Microsoft.EntityFrameworkCore.Query
+{
+    public class NorthwindODataQueryTestFixture : NorthwindQuerySqlServerFixture<NoopModelCustomizer>, IODataQueryTestFixture
+    {
+        private IHost _selfHostServer = null;
+
+        protected override string StoreName { get; } = "ODataNorthwind";
+
+        public NorthwindODataQueryTestFixture()
+        {
+            var controllers = new Type[]
+            {
+                typeof(CustomersController),
+                typeof(OrdersController),
+                typeof(OrderDetailsController),
+                typeof(EmployeesController),
+                typeof(ProductsController),
+            };
+
+            (BaseAddress, ClientFactory, _selfHostServer)
+                = ODataQueryTestFixtureInitializer.Initialize<NorthwindODataContext>(StoreName, controllers, GetEdmModel());
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var modelBuilder = new ODataConventionModelBuilder();
+            modelBuilder.EntitySet<Customer>("Customers");
+            modelBuilder.EntitySet<Order>("Orders");
+            modelBuilder.EntityType<OrderDetail>().HasKey(e => new { e.OrderID, e.ProductID });
+            modelBuilder.EntitySet<OrderDetail>("Order Details");
+
+            return modelBuilder.GetEdmModel();
+        }
+
+        public string BaseAddress { get; private set; }
+
+        public IHttpClientFactory ClientFactory { get; private set; }
+
+        public override async Task DisposeAsync()
+        {
+            if (_selfHostServer != null)
+            {
+                await _selfHostServer.StopAsync();
+                _selfHostServer.Dispose();
+                _selfHostServer = null;
+            }
+        }
+    }
+}
---
+++ test/EFCore.OData.FunctionalTests/Query/ODataQueryTestFixtureInitializer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Batch;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNet.OData.Routing.Conventions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OData.Edm;
+
+namespace Microsoft.EntityFrameworkCore.Query
+{
+    public class ODataQueryTestFixtureInitializer
+    {
+        public static (string BaseAddress, IHttpClientFactory ClientFactory, IHost SelfHostServer) Initialize<TContext>(
+            string storeName,
+            Type[] controllers,
+            IEdmModel edmModel)
+            where TContext : DbContext
+        {
+            var clientFactory = default(IHttpClientFactory);
+            var selfHostServer = Host.CreateDefaultBuilder()
+                .ConfigureServices(services => services.AddSingleton<IHostLifetime, NoopHostLifetime>())
+                .ConfigureWebHostDefaults(webBuilder => webBuilder
+                    .UseKestrel(options => options.Listen(IPAddress.Loopback, 0))
+                    .ConfigureServices(services =>
+                    {
+                        services.AddHttpClient();
+                        services.AddOData();
+                        services.AddRouting();
+
+                        UpdateConfigureServices<TContext>(services, storeName);
+                    })
+                    .Configure(app =>
+                    {
+                         clientFactory = app.ApplicationServices.GetRequiredService<IHttpClientFactory>();
+
+                        app.UseODataBatching();
+                        app.UseRouting();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            var config = new EndpointRouteConfiguration(endpoints);
+                            UpdateConfigure(config, controllers, edmModel);
+                        });
+                    })
+                    .ConfigureLogging((hostingContext, logging) =>
+                    {
+                        logging.AddDebug();
+                        logging.SetMinimumLevel(LogLevel.Warning);
+                    }
+                )).Build();
+
+            selfHostServer.Start();
+
+            var baseAddress = selfHostServer.Services.GetService<IServer>().Features.Get<IServerAddressesFeature>().Addresses.First();
+
+            return (baseAddress, clientFactory, selfHostServer);
+        }
+
+        public static void UpdateConfigureServices<TContext>(IServiceCollection services, string storeName)
+            where TContext : DbContext
+        {
+            services.AddDbContext<TContext>(b =>
+                b.UseSqlServer(
+                    SqlServerTestStore.CreateConnectionString(storeName)));
+        }
+
+        protected static void UpdateConfigure(EndpointRouteConfiguration configuration, Type[] controllers, IEdmModel edmModel)
+        {
+            configuration.AddControllers(controllers);
+            configuration.MaxTop(2).Expand().Select().OrderBy().Filter();
+
+            configuration.MapODataRoute("odata", "odata",
+                edmModel,
+                new DefaultODataPathHandler(),
+                ODataRoutingConventions.CreateDefault(),
+                new DefaultODataBatchHandler());
+        }
+
+        private class NoopHostLifetime : IHostLifetime
+        {
+            public Task StopAsync(CancellationToken cancellationToken)
+            {
+                return Task.CompletedTask;
+            }
+
+            public Task WaitForStartAsync(CancellationToken cancellationToken)
+            {
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
+
